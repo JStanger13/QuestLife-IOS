b1bfd8a05fb206ac4315fcf91cb3296ba3252fa9
Justin Stanger <Justin.d.stanger@gmail.com>
2018-05-10 15:27:21 -0400

    Initial Commit

---
+++ QuestLife/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuestLife
+//
+//  Created by Justin Stanger on 5/3/18.
+//  Copyright © 2018 Justin Stanger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
